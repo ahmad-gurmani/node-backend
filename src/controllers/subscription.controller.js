18b9f4913a41ce79d2b2779599e4e4c42194f72f
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-04-23 01:22:01 +0500

    change some subscription routes settings and add business logic for getUserChannelSubscribers endpoint

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -45,11 +45,52 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const { channelId } = req.params
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel ID")
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber"
+            }
+        },
+        {
+            $addFields: {
+                subscriber: { $arrayElemAt: ["$subscriber", 0] }
+            }
+        },
+        {
+            $project: {
+                subscriber: {
+                    _id: 1,
+                    username: 1,
+                    email: 1,
+                    avatar: 1
+                }
+            }
+        }
+    ])
+
+    if (!subscribers) {
+        throw new ApiError(404, "No subscribers found")
+    }
+
+    return res.status(200).json(new ApiResponse(200, subscribers, "Subscribers"))
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+    const { subscriberId } = req.params;
 })
 
 export {
