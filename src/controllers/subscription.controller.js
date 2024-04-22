c9626f50a312b65256beb5fb980da7ae6e3449f7
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-04-23 01:49:10 +0500

    add getSubscribedChannels endpoint logic

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -90,7 +90,52 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params;
+    const { _id } = req.user;
+
+    if (!isValidObjectId(_id)) {
+        throw new ApiError(400, "Invalid user ID")
+    }
+
+    const channels = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(_id)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channel"
+            }
+        },
+        {
+            $addFields: {
+                channel: { $arrayElemAt: ["$channel", 0] }
+            }
+        },
+        {
+            $project: {
+                channel: {
+                    _id: 1,
+                    username: 1,
+                    email: 1,
+                    avatar: 1,
+                }
+            }
+        }
+    ])
+
+
+    if (!channels) {
+        throw new ApiError(404, "No channels found")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channels, "Subscribed channels"))
+
 })
 
 export {
