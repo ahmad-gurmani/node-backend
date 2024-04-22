da639eba1c58c6b4c7ba1504e94061c1636f7b06
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-04-22 21:08:41 +0500

    toggleSubscription api business logic complete

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -5,11 +5,42 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
-
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const { channelId } = req.params
-    // TODO: toggle subscription
-})
+    const { channelId } = req.params;
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    if (req.user._id.toString() === channelId.toString()) {
+        throw new ApiError(400, "You can't subscribe to your own channel");
+    }
+
+    // Check if the user is already subscribed to the channel
+    const subscription = await Subscription.findOne({
+        channel: channelId,
+        subscriber: req.user._id
+    });
+
+    if (subscription) {
+        // If already subscribed, unsubscribe
+        await Subscription.findByIdAndDelete(subscription._id);
+        return res.status(200).json(new ApiResponse(200, "Subscription removed"));
+    }
+
+    // If not subscribed, subscribe to the channel
+    const subscribed = await Subscription.create({
+        channel: channelId,
+        subscriber: req.user._id
+    });
+
+    if (!subscribed) {
+        throw new ApiError(500, "Unable to subscribe to this channel");
+    }
+
+    return res.status(200).json(new ApiResponse(200, subscribed, "Subscription added"));
+});
+
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
