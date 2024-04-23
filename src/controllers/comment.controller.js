05c7fe540809357c6f08831cb8d62ff2cccb5b49
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-04-24 02:45:14 +0500

    updateComment api business logic completes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -41,6 +41,45 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     //Todo: upadte a comment
+    const { commentId } = req.params;
+    const { content } = req.body;
+    const { _id } = req.user;
+
+    if (!commentId) {
+        throw new ApiError(400, "Invalid comment ID")
+    }
+
+    // ensure comment is not empty
+    if (!content || content.trim() === '') {
+        throw new ApiError(400, "Comment cannot be empty")
+    }
+
+    const comment = await Comment.findByIdAndUpdate(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+    if (comment.owner.toString() !== _id.toString()) {
+        throw new ApiError(401, "You are not authorized to update this comment")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId,
+        {
+            $set: { content }
+        },
+        { new: true }
+    )
+
+    if (!updatedComment) {
+        throw new ApiError(500, "Failed to update comment")
+    }
+
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedComment, "Comment updated successfully"))
+
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
